54ba1213756b734a0a1f138cb7e36cf524f5e5bc
Gilberto Alvarado <gilberto@sawyereffect.com>
2016-12-22 10:51:49 -0500

    [NAG-116] Files should be uploaded to 'data/incoming' directory

--- app/controllers/import_controller.rb
+++ app/controllers/import_controller.rb
@@ -7,7 +7,7 @@ class ImportController < ApplicationController
   def upload
     filename = params[:import_file]
     puts filename
-    File.open(Rails.root.join('public', 'upload', filename.original_filename), 'wb') do |file|
+    File.open(Rails.root.join('data', 'incoming', filename.original_filename), 'wb') do |file|
       file.write(filename.read)
     end unless filename.to_s.empty?
   end
