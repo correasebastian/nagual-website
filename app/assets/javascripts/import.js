3306368d64dee4f7b9868cb122bdeb71030a8917
Gilberto Alvarado <gilberto@sawyereffect.com>
2016-12-22 11:30:37 -0500

    [NAG-116] Import CSV Javascript module. Function to open file browse to upload CSV.

--- app/assets/javascripts/import.js
+++ app/assets/javascripts/import.js
@@ -1,1 +1,25 @@
-// add your JS here
+var app = {};
+
+(function(app) {
+
+  var importer = {
+    getFile: getFile
+  };
+
+  function getFile() {
+    var importBox = $('#import_file');
+    var fileBrowse = $('#file-browse');
+    fileBrowse.click(function (e) {
+      if (importBox) {
+        importBox.click();
+      }
+      e.preventDefault();
+    });
+  }
+
+  app.nagual = importer;
+
+})(app);
+
+app.nagual.getFile();
+
