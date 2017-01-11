01321e48d0d253c20ffaa577c9a38f91f15d2279
Martha  Fernández <marthafernandez@MacBook-Pro-de-Martha.local>
2017-01-11 16:14:19 -0600

    drag box, have animation when you drop a file

--- app/assets/javascripts/import.js
+++ app/assets/javascripts/import.js
@@ -1,85 +1,84 @@
 var app = {
-  nagual: {}
+    nagual: {}
 };
+(function (app) {
+    var importer = {
+        init: init
+    };
 
-(function(app) {
+    function initDragFile() {
+        var importFileArea = $('#import-area');
+        importFileArea.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+        });
+        importFileArea.on('dragover dragenter', function (e) {
+            e.preventDefault();
+            e.stopPropagation();
+            importFileArea.addClass("upLoading");
+        });
+        importFileArea.on('dragleave dragend drop', function (e) {
+            if (e.originalEvent.dataTransfer) {
+                files = e.originalEvent.dataTransfer.files;
+                importFileArea.removeClass("upLoading");
+                if (files.length) {
+                    e.preventDefault();
+                    e.stopPropagation();
+                    upload(files[0]);
+                }
+            }
+        });
+    }
 
-  var importer = {
-    init: init
-  };
+    function initUploadFile() {
+        var importBox = $('#import-file');
+        var fileBrowse = $('#file-browse');
+        fileBrowse.click(function (e) {
+            if (importBox) {
+                importBox.click();
+            }
+            e.preventDefault();
+        });
+        importBox.change(function (e) {
+            file = importBox[0].files[0];
+            upload(file);
+        });
+    };
+    $("#importStatus").hide();
 
-  function initDragFile() {
-    var importFileArea = $('#import-area');
+    function upload(file) {
+        var formData = new FormData();
+        formData.append('import_file', file);
+        //when the file is loading... 
+        $("#importStatus").show().addClass("loadingImport");
+        $("#import-area").remove();
+        $("#importStatus h2").text("Loading file…");
+        $.ajax({
+            url: 'import/upload'
+            , data: formData
+            , processData: false
+            , contentType: false
+            , type: 'POST'
+            , success: function (data) {
+                //alert("File successfully uploaded");
+                $("#importStatus").show().addClass("successImport");
+                $("#importStatus h2").text("Loaded correctly");
+            }
+            , error: function (data) {
+                //alert("Error uploading file");
+                $("#importStatus").show().addClass("errorImport");
+                $("#importStatus h2").text("Error uploading file");
+                $("#importStatus p").text("Try again.");
+            }
+        });
+    }
 
-    importFileArea.on( 'dragover', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-    });
-
-    importFileArea.on( 'dragenter', function(e) {
-      e.preventDefault();
-      e.stopPropagation();
-    });
-
-    importFileArea.on( 'drop', function(e) {
-
-      if(e.originalEvent.dataTransfer){
-        files = e.originalEvent.dataTransfer.files;
-        if(files.length) {
-          e.preventDefault();
-          e.stopPropagation();
-          upload(files[0]);
-        }
-      }
-    });
-
-  }
-
-  function initUploadFile() {
-    var importBox = $('#import-file');
-    var fileBrowse = $('#file-browse');
-
-    fileBrowse.click(function (e) {
-      if (importBox) {
-        importBox.click();
-      }
-      e.preventDefault();
-    });
-
-    importBox.change(function(e) {
-      file = importBox[0].files[0];
-      upload(file);
-    });
-
-  };
-
-  function upload(file) {
-    var formData = new FormData();
-    formData.append('import_file', file);
-
-    $.ajax({
-      url: 'import/upload',
-      data: formData,
-      processData: false,
-      contentType: false,
-      type: 'POST',
-      success: function(data){
-        alert("File successfully uploaded");
-      }, error: function(data){
-        alert("Error uploading file");
-      }
-    });
-  }
-
-  function init() {
-    initUploadFile();
-    initDragFile();
-  }
-
-  app.nagual.importer = importer;
-
+    function init() {
+        initUploadFile();
+        initDragFile();
+    }
+    app.nagual.importer = importer;
 })(app);
-
 $(function () {
-  app.nagual.importer.init();
+    app.nagual.importer.init();
 });
