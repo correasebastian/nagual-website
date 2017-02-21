be196878bf47d52d23250d427ea7f0fc9e86e8b7
Gilberto Alvarado <gilberto@sawyereffect.com>
2017-02-21 12:02:08 -0500

    [NAG-180] Authenticate all URLs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,8 @@
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
 
-  before_action :authenticate
+  before_action :authenticate_user!
+
+  protect_from_forgery with: :exception
 
   def data
     filename = "#{params[:file]}.#{params[:format]}"
@@ -9,14 +10,4 @@ class ApplicationController < ActionController::Base
     send_file Rails.root.join('data', filename)
   end
 
-  private
-
-  def authenticate
-    return unless Rails.env.production?
-
-    authenticate_or_request_with_http_basic do |username, password|
-      username == Rails.application.secrets.auth_user &&
-        password == Rails.application.secrets.auth_password
-    end
-  end
 end
