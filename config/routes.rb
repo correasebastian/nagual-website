c9c3bb1985e7a1bfbce1203804a5de3aed05c291
Gilberto Alvarado <gilberto@sawyereffect.com>
2017-02-21 11:17:48 -0500

    [NAG-180] User model and migration for authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+
   get 'export/index'
 
   get  'import/index'
--- db/schema.rb
+++ db/schema.rb
@@ -10,86 +10,100 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_170_117_150_519) do
+ActiveRecord::Schema.define(version: 20170221155127) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'custom_attributes', force: :cascade do |t|
-    t.string   'product_id'
-    t.string   'name'
-    t.string   'value'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index %w(product_id name), name: 'index_custom_attributes_on_product_id_and_name', unique: true, using: :btree
-    t.index ['product_id'], name: 'index_custom_attributes_on_product_id', using: :btree
+  create_table "custom_attributes", force: :cascade do |t|
+    t.string   "product_id"
+    t.string   "name"
+    t.string   "value"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id", "name"], name: "index_custom_attributes_on_product_id_and_name", unique: true, using: :btree
+    t.index ["product_id"], name: "index_custom_attributes_on_product_id", using: :btree
   end
 
-  create_table 'job_history_entries', force: :cascade do |t|
-    t.string   'processed_file_path'
-    t.integer  'processed_records'
-    t.string   'status'
-    t.string   'report_file_path'
-    t.datetime 'created_at',          null: false
-    t.datetime 'updated_at',          null: false
+  create_table "job_history_entries", force: :cascade do |t|
+    t.string   "processed_file_path"
+    t.integer  "processed_records"
+    t.string   "status"
+    t.string   "report_file_path"
+    t.datetime "created_at",          null: false
+    t.datetime "updated_at",          null: false
   end
 
-  create_table 'prices', force: :cascade do |t|
-    t.string   'product_id'
-    t.decimal  'price', precision: 15, scale: 4
-    t.datetime 'created_at',                          null: false
-    t.datetime 'updated_at',                          null: false
-    t.index ['product_id'], name: 'index_prices_on_product_id', using: :btree
+  create_table "prices", force: :cascade do |t|
+    t.string   "product_id"
+    t.decimal  "price",      precision: 15, scale: 4
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["product_id"], name: "index_prices_on_product_id", using: :btree
   end
 
-  create_table 'products', force: :cascade do |t|
-    t.string   'product_id'
-    t.string   'ean'
-    t.string   'upc'
-    t.string   'unit'
-    t.integer  'min_order_quantity'
-    t.integer  'step_quantity'
-    t.string   'display_name'
-    t.string   'short_description'
-    t.text     'long_description'
-    t.boolean  'online_flag'
-    t.datetime 'online_from'
-    t.datetime 'online_to'
-    t.boolean  'available_flag'
-    t.string   'template'
-    t.string   'tax_class_id'
-    t.string   'brand'
-    t.string   'manufacturer_name'
-    t.string   'manufacturer_sku'
-    t.datetime 'created_at',         null: false
-    t.datetime 'updated_at',         null: false
-    t.string   'master_product_id'
-    t.string   'type'
-    t.index ['product_id'], name: 'index_products_on_product_id', unique: true, using: :btree
+  create_table "products", force: :cascade do |t|
+    t.string   "product_id"
+    t.string   "ean"
+    t.string   "upc"
+    t.string   "unit"
+    t.integer  "min_order_quantity"
+    t.integer  "step_quantity"
+    t.string   "display_name"
+    t.string   "short_description"
+    t.text     "long_description"
+    t.boolean  "online_flag"
+    t.datetime "online_from"
+    t.datetime "online_to"
+    t.boolean  "available_flag"
+    t.string   "template"
+    t.string   "tax_class_id"
+    t.string   "brand"
+    t.string   "manufacturer_name"
+    t.string   "manufacturer_sku"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "master_product_id"
+    t.string   "type"
+    t.index ["product_id"], name: "index_products_on_product_id", unique: true, using: :btree
   end
 
-  create_table 'stocks', force: :cascade do |t|
-    t.string   'product_id'
-    t.boolean  'perpetual'
-    t.integer  'stock'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.index ['product_id'], name: 'index_stocks_on_product_id', using: :btree
+  create_table "stocks", force: :cascade do |t|
+    t.string   "product_id"
+    t.boolean  "perpetual"
+    t.integer  "stock"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["product_id"], name: "index_stocks_on_product_id", using: :btree
   end
 
-  create_table 'variation_attribute_values', force: :cascade do |t|
-    t.string 'variation_attribute_id'
-    t.string 'value'
-    t.string 'display_value'
-    t.index %w(variation_attribute_id value), name: 'var_attr_vals_index', unique: true, using: :btree
-    t.index ['variation_attribute_id'], name: 'index_variation_attribute_values_on_variation_attribute_id', using: :btree
+  create_table "users", force: :cascade do |t|
+    t.string   "email",               default: "", null: false
+    t.string   "encrypted_password",  default: "", null: false
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",       default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.inet     "current_sign_in_ip"
+    t.inet     "last_sign_in_ip"
+    t.datetime "created_at",                       null: false
+    t.datetime "updated_at",                       null: false
+    t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
-  create_table 'variation_attributes', force: :cascade do |t|
-    t.string 'product_id'
-    t.string 'attribute_id'
-    t.string 'variation_attribute_id'
-    t.string 'display_value'
-    t.index %w(product_id attribute_id), name: 'index_variation_attributes_on_product_id_and_attribute_id', unique: true, using: :btree
-    t.index ['product_id'], name: 'index_variation_attributes_on_product_id', using: :btree
+  create_table "variation_attribute_values", force: :cascade do |t|
+    t.string "variation_attribute_id"
+    t.string "value"
+    t.string "display_value"
+    t.index ["variation_attribute_id", "value"], name: "var_attr_vals_index", unique: true, using: :btree
+    t.index ["variation_attribute_id"], name: "index_variation_attribute_values_on_variation_attribute_id", using: :btree
   end
+
+  create_table "variation_attributes", force: :cascade do |t|
+    t.string "attribute_id"
+    t.string "variation_attribute_id"
+    t.string "display_value"
+    t.string "product_id"
+  end
+
 end
