4b7b08d9a86881d817234b5ea26f9f73a95ecc2c
sebastian correa morales <sebastian@sawyereffect.com>
2017-04-28 14:14:39 -0400

    refactor(aws API): [NAG-403] simplified api for testing purpose

--- api/src/model/contact-us/controller.js
+++ api/src/model/contact-us/controller.js
@@ -1,6 +1,6 @@
 const util = require('util');
-const rp = require('request-promise-native');
-const config = require('../../config');
+// const rp = require('request-promise-native');
+// const config = require('../../config');
 
 function onPost(req, res, next) {
   req.checkBody('email_address', 'must include email property').notEmpty();
@@ -14,29 +14,31 @@ function onPost(req, res, next) {
         });
         return;
       }
-      const options = {
-        method: 'POST',
-        uri: `https://us1.api.mailchimp.com/3.0/lists/${config.mailchimp.list}/members`,
-        body: {
-          email_address: req.body.email_address,
-          status: 'subscribed'
-        },
-        headers:{
-          Authorization: `apikey ${config.mailchimp.apiKey}`
-        },
-        json: true // Automatically stringifies the body to JSON
-      };
 
-      rp(options)
-        .then((parsedBody) => {
-          // POST succeeded...
-          res.status(201).json(parsedBody);
-        })
-        .catch((err) => {
-          // console.log('error', err);
-          // POST failed...
-          res.status(err.statusCode).json(err.error);
-        });
+      res.status(201).json(req.body);
+      /*      const options = {
+              method: 'POST',
+              uri: `https://us1.api.mailchimp.com/3.0/lists/${config.mailchimp.list}/members`,
+              body: {
+                email_address: req.body.email_address,
+                status: 'subscribed'
+              },
+              headers:{
+                Authorization: `apikey ${config.mailchimp.apiKey}`
+              },
+              json: true // Automatically stringifies the body to JSON
+            };
+
+            rp(options)
+              .then((parsedBody) => {
+                // POST succeeded...
+                res.status(201).json(parsedBody);
+              })
+              .catch((err) => {
+                // console.log('error', err);
+                // POST failed...
+                res.status(err.statusCode).json(err.error);
+              });*/
 
     });
 }
